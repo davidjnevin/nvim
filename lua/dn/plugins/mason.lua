9ba017fa0b4d2e8c88ab7d2670c242cb121cc55b
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-01-02 18:13:09 +0100

    Add linting

--- lua/dn/plugins/mason.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    "williamboman/mason.nvim",
-    build = ":MasonUpdate", -- :MasonUpdate updates registry contents
-  },
-  "williamboman/mason-lspconfig.nvim",
-  "jay-babu/mason-null-ls.nvim",
-}
