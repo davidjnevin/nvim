63ce8c5c107b103683ff9052f9046576dd9134db
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-01-11 22:02:51 +0100

    Add trouble for diagnostics

---
+++ lua/dn/plugins/trouble.lua
@@ -0,0 +1,10 @@
+return {
+ "folke/trouble.nvim",
+config = function()
+	require("trouble").setup {
+		icons = false,
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	}
+end,
+}
