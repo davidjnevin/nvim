b8ebecfe2b7ea754a792f78b60a49a8cadd38f0f
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-02-10 21:03:58 +0100

    add jsonls, tsserver, cssls, html, eslint
    for better react development env

--- lua/dn/plugins/lsp/mason.lua
+++ lua/dn/plugins/lsp/mason.lua
@@ -25,9 +25,14 @@ return {
 		mason_lspconfig.setup({
 			-- list of servers for mason to install
 			ensure_installed = {
+				"jsonls",
 				"lua_ls",
 				"jedi_language_server",
 				"taplo",
+				"tsserver",
+				"cssls",
+				"html",
+				"eslint",
 			},
 			-- auto-install configured servers (with lspconfig)
 			automatic_installation = true, -- not the same as ensure_installed
