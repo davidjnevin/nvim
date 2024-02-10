954559e51c0a9a254c2b29d39876efa48c34db2a
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-02-10 21:59:06 +0100

    move lazygit setup to lazy.lua

--- init.lua
+++ init.lua
@@ -10,19 +10,8 @@ vim.g.bulitin_lsp = true
 
 vim.g.pydocstring_doq_path = "/Library/Frameworks/Python.framework/Versions/3.11/bin/doq"
 -- Plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system {
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	}
-end
-vim.opt.rtp:prepend(lazypath)
 require("dn.lazy")
+
 require("toggleterm").setup {}
 
 -- Lazygit setup
