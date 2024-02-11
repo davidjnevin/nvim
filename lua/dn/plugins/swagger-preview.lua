bb01e6fccb0f927e941dab50a9d5719438eb50be
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-02-11 01:49:57 +0100

    unused plugin

--- lua/dn/plugins/swagger-preview.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "vinnymeller/swagger-preview.nvim",
-  build = "npm install -g swagger-ui-watcher",
-  opts = {
-    port = 8000,
-    host = "localhost",
-  },
-  cmd = { "SwaggerPreview", "SwaggerPreviewToggle" },
-}
