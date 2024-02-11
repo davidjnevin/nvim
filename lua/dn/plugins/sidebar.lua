5b6ac3168d0d41f3c6b66fa66a751e8162ab25f6
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-02-11 01:14:16 +0100

    temp diabale to see if useful

--- lua/dn/plugins/sidebar.lua
+++ lua/dn/plugins/sidebar.lua
@@ -1,11 +1,11 @@
 return {
-    "sidebar-nvim/sidebar.nvim",
-    config = function()
-        require("sidebar-nvim").setup({
-            ["git"] = {
-                icon = "",
-            },
-            sections = { "datetime", "git" },
-        })
-    end,
+    -- "sidebar-nvim/sidebar.nvim",
+    -- config = function()
+    --     require("sidebar-nvim").setup({
+    --         ["git"] = {
+    --             icon = "",
+    --         },
+    --         sections = { "datetime", "git" },
+    --     })
+    -- end,
 }
