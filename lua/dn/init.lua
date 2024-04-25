39f07bc29cc6584328c9ea96fdb23b494349572e
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-04-26 01:52:30 +0200

    add keymap descriptions

--- lua/dn/init.lua
+++ lua/dn/init.lua
@@ -45,17 +45,16 @@ autocmd({"BufWritePre"}, {
 autocmd('LspAttach', {
     group = DnGroup,
     callback = function(e)
-        local opts = { buffer = e.buf }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>lws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>lca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>lrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>lrn", function() vim.lsp.buf.renare() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, { buffer = e.buf, desc = "Go to definition" })
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, { buffer = e.buf, desc = "Show documentation" })
+		vim.keymap.set("n", "<leader>lws", function() vim.lsp.buf.workspace_symbol() end, { buffer = e.buf, desc = "Lists all symbol"})
+        vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, { buffer = e.buf, desc = "Show contents in floating window"})
+        vim.keymap.set("n", "<leader>lca", function() vim.lsp.buf.code_action() end, { buffer = e.buf, desc = "Show code actions"})
+        vim.keymap.set("n", "<leader>lrr", function() vim.lsp.buf.references() end, { buffer = e.buf, desc = "Show references"})
+        vim.keymap.set("n", "<leader>lrn", function() vim.lsp.buf.rename() end, { buffer = e.buf, desc = "Rename reference"})
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, { buffer = e.buf, desc = "Signature help"})
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, { buffer = e.buf, desc = "Go to next diagnostic"})
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, { buffer = e.buf, desc = "Go to prev diagnostic"})
     end
 })
 
