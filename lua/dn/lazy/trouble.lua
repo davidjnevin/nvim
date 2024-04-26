b33e77957b3d12cc7b9b2e707c4849c328b0fa4b
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-04-26 12:42:51 +0200

    add keymap descriptions

--- lua/dn/lazy/trouble.lua
+++ lua/dn/lazy/trouble.lua
@@ -5,8 +5,8 @@ return {
 			icons= false,
 		})
 
-		vim.keymap.set("n", "<leader>tt", function() require("trouble").toggle() end)
-		vim.keymap.set("n", "<leader>[t", function() require("trouble").next({skip_groups = true, jump = true}); end)
-		vim.keymap.set("n", "<leader>]t", function() require("trouble").previous({skip_groups = true, jump = true}); end)
+		vim.keymap.set("n", "<leader>tt", function() require("trouble").toggle() end, { desc = "Toggle trouble" } )
+		vim.keymap.set("n", "<leader>[t", function() require("trouble").next({skip_groups = true, jump = true}); end, { desc = "Next trouble" } )
+		vim.keymap.set("n", "<leader>]t", function() require("trouble").previous({skip_groups = true, jump = true}); end, { desc = "Prev trouble" } )
 	end,
 }
