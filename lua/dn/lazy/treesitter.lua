9c83cb3b0ac16820e99ab54e57e1c37a8e379054
David J Nevin <nevin.david@gmail.com>
2024-11-12 12:34:54 +0100

    incremental selection keymaps

--- lua/dn/lazy/treesitter.lua
+++ lua/dn/lazy/treesitter.lua
@@ -1,106 +1,105 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- A list of parser names, or "all"
-			ensure_installed = {
-				"bash",
-				"c",
-				"make",
-				"css",
-				"html",
-				"javascript",
-				"json",
-				"lua",
-				"python",
-				"query",
-				"sql",
-				"templ",
-				"tmux",
-				"toml",
-				"vim",
-				"vimdoc",
-				"yaml",
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            -- A list of parser names, or "all"
+            ensure_installed = {
+                "bash",
+                "c",
+                "make",
+                "css",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "python",
+                "query",
+                "sql",
+                "templ",
+                "tmux",
+                "toml",
+                "vim",
+                "vimdoc",
+                "yaml",
                 "java",
-			},
+            },
 
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = true,
+            -- Install parsers synchronously (only applied to `ensure_installed`)
+            sync_install = true,
 
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = true,
+            -- Automatically install missing parsers when entering buffer
+            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            auto_install = true,
 
-			-- indent = {
-			-- 	disable = { 'yaml' },
-			-- 	enable = false
-			-- },
+            -- indent = {
+            -- 	disable = { 'yaml' },
+            -- 	enable = false
+            -- },
 
-			highlight = {
-				-- `false` will disable the whole extension
-				enable = true,
+            highlight = {
+                -- `false` will disable the whole extension
+                enable = true,
 
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = { "markdown", },
-			},
+                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
+                -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                -- Instead of true it can also be a list of languages
+                additional_vim_regex_highlighting = { "markdown", },
+            },
 
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<Leader>sn",
-					node_incremental = "<Leader>sk",
-					node_decremental = "<Leader>sj",
-					scope_incremental = "<Leader>sc",
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<Leader>sn",
+                    node_incremental = "<Leader>sk",
+                    node_decremental = "<Leader>sj",
+                    scope_incremental = "<Leader>sc",
+                },
+            },
+            textobjects = {
+                select = {
+                    enable = true,
 
-				},
-			},
-			textobjects = {
-				select = {
-					enable = true,
+                    -- Automatically jump forward to textobj, similar to targets.vim
+                    lookahead = true,
 
-					-- Automatically jump forward to textobj, similar to targets.vim
-					lookahead = true,
-
-					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
-						["af"] = { query = "@function.outer", desc = "Select around a function region" },
-						["if"] = { query = "@function.inner", desc = "Select inner part of a function region" },
-						["ac"] = { query = "@class.outer", desc = "Select around a class region" },
-						-- You can optionally set descriptions to the mappings (used in the desc parameter of
-						-- nvim_buf_set_keymap) which plugins like which-key display
-						["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-						-- You can also use captures from other query groups like `locals.scm`
-						["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-					},
-					-- You can choose the select mode (default is charwise 'v')
-					--
-					-- Can also be a function which gets passed a table with the keys
-					-- * query_string: eg '@function.inner'
-					-- * method: eg 'v' or 'o'
-					-- and should return the mode ('v', 'V', or '<c-v>') or a table
-					-- mapping query_strings to modes.
-					selection_modes = {
-						['@parameter.outer'] = 'v', -- charwise
-						-- ['@function.outer'] = 'V', -- linewise
-						['@function.outer'] = 'v', -- charwise
-						['@class.outer'] = '<c-v>', -- blockwise
-					},
-					-- If you set this to `true` (default is `false`) then any textobject is
-					-- extended to include preceding or succeeding whitespace. Succeeding
-					-- whitespace has priority in order to act similarly to eg the built-in
-					-- `ap`.
-					--
-					-- Can also be a function which gets passed a table with the keys
-					-- * query_string: eg '@function.inner'
-					-- * selection_mode: eg 'v'
-					-- and should return true or false
-					include_surrounding_whitespace = false,
-				},
-			},
-		})
-	end,
+                    keymaps = {
+                        -- You can use the capture groups defined in textobjects.scm
+                        ["af"] = { query = "@function.outer", desc = "Select around a function region" },
+                        ["if"] = { query = "@function.inner", desc = "Select inner part of a function region" },
+                        ["ac"] = { query = "@class.outer", desc = "Select around a class region" },
+                        -- You can optionally set descriptions to the mappings (used in the desc parameter of
+                        -- nvim_buf_set_keymap) which plugins like which-key display
+                        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                        -- You can also use captures from other query groups like `locals.scm`
+                        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                    },
+                    -- You can choose the select mode (default is charwise 'v')
+                    --
+                    -- Can also be a function which gets passed a table with the keys
+                    -- * query_string: eg '@function.inner'
+                    -- * method: eg 'v' or 'o'
+                    -- and should return the mode ('v', 'V', or '<c-v>') or a table
+                    -- mapping query_strings to modes.
+                    selection_modes = {
+                        ['@parameter.outer'] = 'v', -- charwise
+                        -- ['@function.outer'] = 'V', -- linewise
+                        ['@function.outer'] = 'v', -- charwise
+                        ['@class.outer'] = '<c-v>', -- blockwise
+                    },
+                    -- If you set this to `true` (default is `false`) then any textobject is
+                    -- extended to include preceding or succeeding whitespace. Succeeding
+                    -- whitespace has priority in order to act similarly to eg the built-in
+                    -- `ap`.
+                    --
+                    -- Can also be a function which gets passed a table with the keys
+                    -- * query_string: eg '@function.inner'
+                    -- * selection_mode: eg 'v'
+                    -- and should return true or false
+                    include_surrounding_whitespace = false,
+                },
+            },
+        })
+    end,
 }
