7149d25517a22ec5ab9c0101d403d42498ebcc73
David J Nevin <nevin.david@gmail.com>
2024-12-03 08:28:11 +0100

    comment out setup call

--- lua/dn/lazy/trouble.lua
+++ lua/dn/lazy/trouble.lua
@@ -1,12 +1,12 @@
 return {
-	"folke/trouble.nvim",
-	config = function()
-		require("trouble").setup ({
-			icons= false,
-		})
+    "folke/trouble.nvim",
+        config = function()
+               -- require("trouble").setup ({
+               --         icons= false,
+               -- })
 
-		vim.keymap.set("n", "<leader>tt", function() require("trouble").toggle() end, { desc = "Toggle trouble" } )
-		vim.keymap.set("n", "<leader>[t", function() require("trouble").next({skip_groups = true, jump = true}); end, { desc = "Next trouble" } )
-		vim.keymap.set("n", "<leader>]t", function() require("trouble").previous({skip_groups = true, jump = true}); end, { desc = "Prev trouble" } )
-	end,
+               vim.keymap.set("n", "<leader>tt", function() require("trouble").toggle() end, { desc = "Toggle trouble" } )
+               vim.keymap.set("n", "<leader>[t", function() require("trouble").next({skip_groups = true, jump = true}); end, { desc = "Next trouble" } )
+               vim.keymap.set("n", "<leader>]t", function() require("trouble").previous({skip_groups = true, jump = true}); end, { desc = "Prev trouble" } )
+       end,
 }
